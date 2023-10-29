8ac758a591c9ead9fe6a5f1cec3e509f4d8b4d59
Cael Rowley <drdeus@pm.me>
2023-10-29 19:10:07 +0100

    feat: animate player and kobold enemy

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -3,10 +3,21 @@ extends CharacterBody2D
 
 @export var speed := 20.0
 
+@onready var character_sprite: AnimatedSprite2D = %CharacterSprite
 @onready var player: CharacterBody2D = get_tree().get_first_node_in_group("player")
+
+
+func _ready():
+	character_sprite.play("walk")
 
 
 func _physics_process(_delta: float):
 	var direction := global_position.direction_to(player.global_position)
 	velocity = direction * speed
+	
+	if direction.x > 0.1:
+		character_sprite.flip_h = true
+	elif direction.x < -0.1:
+		character_sprite.flip_h = false
+	
 	move_and_slide()
--- player/player.gd
+++ player/player.gd
@@ -2,9 +2,25 @@ extends CharacterBody2D
 
 @export var speed := 100.0
 
+@onready var character_sprite: AnimatedSprite2D = %CharacterSprite
+
 
 func _physics_process(delta: float):
-	var movement_input := Vector2(Input.get_action_strength("right") - Input.get_action_strength("left"), Input.get_action_strength("down") - Input.get_action_strength("up"))
+	var x_input := Input.get_action_strength("right") - Input.get_action_strength("left")
+	var y_input := Input.get_action_strength("down") - Input.get_action_strength("up")
+	var movement_input := Vector2(x_input, y_input)
+	
+	if movement_input != Vector2.ZERO:
+		if !character_sprite.is_playing():
+			character_sprite.set_frame(1)
+			character_sprite.play()
+	else:
+		if character_sprite.is_playing():
+			character_sprite.stop()
+	
+	if character_sprite.is_playing():
+		character_sprite.flip_h = movement_input.x > 0
+		
 	velocity = movement_input.normalized() * speed
 	move_and_slide()
 
